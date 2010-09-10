ca65e777d5bb178de5cb510c5c973fc0db0e1adf
Mark Paschal <markpasc@markpasc.org>
2010-09-10 00:12:42 -0700

    Add Lua bridge module (which can only make new states so far)

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
-from distutils.core import setup
+#!/usr/bin/env python
 
+from distutils.core import setup, Extension
+
+
+luabject = Extension('village._luabject',
+                     libraries=['lua'],
+                     sources=['src/luabject.c'])
 
 setup(
     name='village',
     version='1.0',
     packages=['village'],
     scripts=['bin/villaged'],
+    ext_modules=[luabject],
 )
