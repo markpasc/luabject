00efa98eda29bf218c1d83a3d06a03bdb422f266
Mark Paschal <markpasc@markpasc.org>
2010-09-11 13:18:08 -0700

    Exercise us some script loading

--- tests/test_luabject.py
+++ tests/test_luabject.py
@@ -1,6 +1,37 @@
-import village._luabject
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
 
-state = village._luabject.new()
-village._luabject.load_script(state, "function foo() prant() end")
 
-print repr(state)
+from village import _luabject
+
+
+class TestDirect(unittest.TestCase):
+
+    def test_new(self):
+        state = _luabject.new()
+        # PyCObject isn't available to assertIsInstance, so:
+        self.assertEqual(type(state).__name__, 'PyCObject')
+
+    def test_load_script(self):
+        state = _luabject.new()
+        _luabject.load_script(state, "")
+
+        # Can load multiple scripts in one state.
+        _luabject.load_script(state, "")
+
+        # Can load a syntactically correct script.
+        state = _luabject.new()
+        _luabject.load_script(state, "function foo() prant() end")
+
+        # Can load multiple syntactically correct scripts in one state.
+        _luabject.load_script(state, "function bar() prant() end")
+
+        # Loading a syntactically incorrect script raises an exception.
+        state = _luabject.new()
+        with self.assertRaises(ValueError):
+            _luabject.load_script(state, "1+1")
+
+        # Can load a syntactically correct script even after loading an incorrect script raises an exception.
+        _luabject.load_script(state, "function foo() prant() end")
