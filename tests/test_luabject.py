26944e2786ccf4cc18b6cffc70361021be2ed492
Mark Paschal <markpasc@markpasc.org>
2010-09-12 13:13:02 -0700

    Test loading functions with the new script loading system

--- tests/test_luabject.py
+++ tests/test_luabject.py
@@ -14,35 +14,27 @@ class TestDirect(unittest.TestCase):
         # PyCObject isn't available to assertIsInstance, so:
         self.assertEqual(type(state).__name__, 'PyCObject')
 
-    def test_load_script(self):
-        state = _luabject.new()
+    def load_script_to_completion(self, state, script):
         thread = _luabject.new_thread(state)
-        _luabject.load_script(thread, "")
+        _luabject.load_script(thread, script)
         _luabject.pump_thread(thread)
         while _luabject.thread_status(thread) == 1:
             _luabject.pump_thread(thread)
 
+    def test_load_script(self):
+        state = _luabject.new()
+        self.load_script_to_completion(state, "")
+
         # Can load multiple scripts in one state.
-        thread = _luabject.new_thread(state)
-        _luabject.load_script(thread, "")
-        _luabject.pump_thread(thread)
-        while _luabject.thread_status(thread) == 1:
-            _luabject.pump_thread(thread)
+        self.load_script_to_completion(state, "")
 
         # Can load a syntactically correct script.
         state = _luabject.new()
-        thread = _luabject.new_thread(state)
-        _luabject.load_script(thread, "function foo() prant() end")
-        _luabject.pump_thread(thread)
-        while _luabject.thread_status(thread) == 1:
-            _luabject.pump_thread(thread)
+        self.load_script_to_completion(state, "function foo() prant() end")
 
         # Can load multiple syntactically correct scripts in one state.
         thread = _luabject.new_thread(state)
-        _luabject.load_script(thread, "function bar() prant() end")
-        _luabject.pump_thread(thread)
-        while _luabject.thread_status(thread) == 1:
-            _luabject.pump_thread(thread)
+        self.load_script_to_completion(state, "function bar() prant() end")
 
         # Loading a syntactically incorrect script raises an exception.
         state = _luabject.new()
@@ -51,11 +43,7 @@ class TestDirect(unittest.TestCase):
             _luabject.load_script(thread, "1+1")
 
         # Can load a syntactically correct script even after a load_script() exception.
-        thread = _luabject.new_thread(state)
-        _luabject.load_script(thread, "function foo() prant() end")
-        _luabject.pump_thread(thread)
-        while _luabject.thread_status(thread) == 1:
-            _luabject.pump_thread(thread)
+        self.load_script_to_completion(state, "function foo() prant() end")
 
         # Loading a syntactically correct script that causes an error works until we evaluate it.
         state = _luabject.new()
@@ -67,32 +55,30 @@ class TestDirect(unittest.TestCase):
                 _luabject.pump_thread(thread)
 
         # Can load a syntactically correct script even after a load_script() exception.
+        self.load_script_to_completion(state, "function foo() prant() end")
+
+    def test_load_function(self):
+        # Trying to load an unknown function raises an exception.
+        state = _luabject.new()
+        self.load_script_to_completion(state, "bar = 1")
+
         thread = _luabject.new_thread(state)
-        _luabject.load_script(thread, "function foo() prant() end")
-        _luabject.pump_thread(thread)
-        while _luabject.thread_status(thread) == 1:
-            _luabject.pump_thread(thread)
-
-    def test_start_function(self):
-        raise unittest.SkipTest()
-
-        # Trying to start an unknown function raises an exception.
-        state = _luabject.new()
-        _luabject.load_script(state, "bar = 1")
         with self.assertRaises(ValueError):
-            _luabject.start_function(state, "foo")
+            _luabject.load_function(thread, "foo")  # nil
         with self.assertRaises(ValueError):
-            _luabject.start_function(state, "bar")
+            _luabject.load_function(thread, "bar")  # not a function
 
         state = _luabject.new()
-        _luabject.load_script(state, "function foo() bar = 1 end")
-        thread = _luabject.start_function(state, "foo")
+        self.load_script_to_completion(state, "function foo() bar = 1 end")
+
+        thread = _luabject.new_thread(state)
+        _luabject.load_function(thread, "foo")
         self.assertEqual(0, _luabject.thread_status(thread))
 
-        # Functions that throw runtime errors can still be started without error.
+        # Functions that throw runtime errors can still be loaded without error.
         state = _luabject.new()
-        _luabject.load_script(state, "function foo() prant() end")
-        thread = _luabject.start_function(state, "foo")
+        self.load_script_to_completion(state, "function foo() prant() end")
+        _luabject.load_function(thread, "foo")
         self.assertEqual(0, _luabject.thread_status(thread))
 
     def test_pump_thread(self):
